6f6e921377a853f7f7384af8286727d92c57caf0
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-08-02 00:11:16 +0100

    add diferent prizes and no collision with obstacles

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ class Player {
         this.height = 40;
         this.positionX = 0;
         this.positionY = 0;
-        this.speed = 5;
+        this.speed = 6;
 
         this.createDomElm();
 
@@ -57,7 +57,7 @@ class Obstacle {
         this.containerWidth = container.offsetWidth;
         this.containerHeigth = container.offsetHeight;
 
-        this.positionX = Math.floor(Math.random() * (400 - this.width + 1));
+        this.positionX = Math.floor(Math.random() * (600 - this.width + 1));
         this.positionY = this.containerHeigth - this.height;
 
         this.createDomElm();
@@ -75,13 +75,89 @@ class Obstacle {
         const container = document.getElementById("container");
         container.appendChild(this.domElement);
         this.newimg = document.createElement('img');
-        this.newimg.setAttribute('src', "./images/close.png");
+        this.newimg.setAttribute('src', "./images/caution.png");
         this.domElement.appendChild(this.newimg);
     }
 
     moveDown() {
         this.positionY--;
         this.domElement.style.bottom = this.positionY + "px";
+    }
+    repositionIfColliding() {
+        for (const obstacleInstance of obstacleArr) {
+            while (isColliding(this, obstacleInstance)) {
+                this.positionX = Math.random() * (600 - this.width);
+                this.positionY = 400;
+                this.domElement.style.left = this.positionX + "px";
+                this.domElement.style.bottom = this.positionY + "px";
+            }
+        }
+    }
+}
+
+class Prize {
+    constructor() {
+        this.width = 40;
+        this.height = 40;
+        this.margin = 10;
+        this.collided = false;
+
+        this.containerWidth = container.offsetWidth;
+        this.containerHeigth = container.offsetHeight;
+
+        this.prizeImg = [
+            './images/lighthouse.png',
+            './images/thunder.png',
+            './images/gambling.png',
+            './images/lightning-bolt.png',
+            './images/label.png',
+            './images/money-bag.png',
+            './images/half-moon.png',
+            './images/umbrella.png',
+        ]
+
+        this.positionX = Math.floor(Math.random() * (600 - this.width + 1));
+        this.positionY = this.containerHeigth - this.height;
+
+        this.prize = this.createDomElm();
+        this.repositionIfColliding();
+    }
+
+    createDomElm() {
+        this.domElement = document.createElement("div");
+        this.domElement.className = "prize";
+        this.domElement.style.left = this.positionX + "px";
+        this.domElement.style.bottom = this.positionY + "px";
+        this.domElement.style.width = this.width + "px";
+        this.domElement.style.heigth = this.height + "px";
+
+
+        const container = document.getElementById("container");
+        container.appendChild(this.domElement);
+
+        const randomImage = this.prizeImg[Math.floor(Math.random() * this.prizeImg.length)];
+
+        this.prizeImg = document.createElement('img');
+        this.prizeImg.setAttribute('src', randomImage);
+        this.domElement.appendChild(this.prizeImg);
+
+        return this.domElement;
+    }
+
+    moveDown() {
+        this.positionY--;
+        this.domElement.style.bottom = this.positionY + "px";
+    }
+    /// reposition of obstacles & prizes
+    repositionIfColliding() {
+        for (const obstacleInstance of obstacleArr) {
+            while (isColliding(this, obstacleInstance)) {
+                this.positionX = Math.random() * (600 - this.width);
+                this.positionY = 400;
+                this.domElement.style.left = this.positionX + "px";
+                this.domElement.style.bottom = this.positionY + "px";
+            }
+        }
     }
 }
 
@@ -90,13 +166,15 @@ class Obstacle {
 const player = new Player();
 const prizeArr = [];
 const obstacleArr = [];
+let score = 0;
+
 
 
 // generate obstacles
 setInterval(() => {
     const newObstacle = new Obstacle();
     obstacleArr.push(newObstacle)
-}, 4000);
+}, 3000);
 
 setInterval(() => {
     obstacleArr.forEach((obstacleInstance) => {
@@ -115,6 +193,64 @@ setInterval(() => {
     });
 }, 50);
 
+/// prizes
+function updateScore() {
+    score++;
+    document.getElementById('score').textContent = `Flashes: ${score}`;
+}
+
+setInterval(() => {
+    const newPrize = new Prize();
+    prizeArr.push(newPrize)
+}, 3000);
+
+setInterval(() => {
+    prizeArr.forEach((prizeInstance) => {
+        prizeInstance.moveDown();
+
+        if (
+            player.positionX < prizeInstance.positionX + prizeInstance.width &&
+            player.positionX + player.width > prizeInstance.positionX &&
+            player.positionY < prizeInstance.positionY + prizeInstance.height &&
+            player.positionY + player.height > prizeInstance.positionY
+        ) {
+            if (!prizeInstance.collided) {
+                updateScore();
+                prizeInstance.collided = true;
+                prizeInstance.prize.remove();
+            }
+
+        }
+    });
+}, 50);
+
+
+/// function to track prizes and obstacles colliding
+
+function isColliding(prize, obstacle) {
+    return (
+        prize.positionX < obstacle.positionX + obstacle.width &&
+        prize.positionX + prize.width > obstacle.positionX &&
+        prize.positionY < obstacle.positionY + obstacle.height &&
+        prize.positionY + prize.height > obstacle.positionY
+    );
+}
+
+function checkPrizeObstacleCollisions() {
+    for (const prizeInstance of prizeArr) {
+        for (const obstacleInstance of obstacleArr) {
+            if (isColliding(prizeInstance, obstacleInstance)) {
+                // Reposition the prize
+                prizeInstance.positionX = Math.floor(Math.random() * (600 - prizeInstance.width));
+                prizeInstance.positionY = 400;
+                prizeInstance.domElement.style.left = prizeInstance.positionX + "px";
+                prizeInstance.domElement.style.bottom = prizeInstance.positionY + "px";
+            }
+        }
+    }
+}
+
+setInterval(checkPrizeObstacleCollisions, 100);
 
 
 
